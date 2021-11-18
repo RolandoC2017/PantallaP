d893e80d17983e02d4392ecc97601bdedf06dd5b
Rolando <rolo2001abril@gmail.com>
2021-11-18 09:45:07 -0600

    login

--- app/src/main/java/com/example/pantallap/MainActivity.kt
+++ app/src/main/java/com/example/pantallap/MainActivity.kt
@@ -74,6 +74,4 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
-
-
 }
